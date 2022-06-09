c5db56ede66a06b5ec478b0d2e28e5433ff53ace
clement peyrat <aiglefeu31@gmail.com>
2022-06-09 15:28:19 +0200

    Seed and index structure done

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,5 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the bin/rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
+Todo.create(title: "Shopping", state: false, user_id: 1)
+Todo.create(title: "Workout", state: false, user_id: 1)
+Todo.create(title: "Eat", state: true, user_id: 1)
+Todo.create(title: "Walk", state: true, user_id: 1)
+Todo.create(title: "Read", state: true, user_id: 1)
