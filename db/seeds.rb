e8ab291584a141b9ad696e1e072b08e576af264c
clement peyrat <aiglefeu31@gmail.com>
2022-06-09 17:19:12 +0200

    Created user in seed

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,4 +1,6 @@
 Todo.destroy_all
+User.destroy_all
+User.create(email: "peyrat.clement@gmail.com", password: "123456")
 Todo.create(title: "Shopping", state: false, user_id: 1, description: "Eggs and rice")
 Todo.create(title: "Workout", state: false, user_id: 1)
 Todo.create(title: "Eat", state: true, user_id: 1)
