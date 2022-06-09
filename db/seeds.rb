16a6296a1325b9bc99c1291177829f457d853478
clement peyrat <aiglefeu31@gmail.com>
2022-06-09 17:22:09 +0200

    Changed seed for todos creation

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,8 +1,8 @@
 Todo.destroy_all
 User.destroy_all
 User.create(email: "peyrat.clement@gmail.com", password: "123456")
-Todo.create(title: "Shopping", state: false, user_id: 1, description: "Eggs and rice")
-Todo.create(title: "Workout", state: false, user_id: 1)
-Todo.create(title: "Eat", state: true, user_id: 1)
-Todo.create(title: "Walk", state: true, user_id: 1)
-Todo.create(title: "Read", state: true, user_id: 1)
+Todo.create(title: "Shopping", state: false, user:, description: "Eggs and rice")
+Todo.create(title: "Workout", state: false, user: User.first)
+Todo.create(title: "Eat", state: true, user: User.first)
+Todo.create(title: "Walk", state: true, user: User.first)
+Todo.create(title: "Read", state: true, user: User.first)
